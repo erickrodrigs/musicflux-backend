4877a7e1adcd74393d9cbbc7506611817d9c8bfb
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2023-02-14 16:55:07 -0300

    start project using spring boot. closes #1.
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>

---
+++ backend/src/test/java/com/erickrodrigues/musicflux/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erickrodrigues.musicflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
