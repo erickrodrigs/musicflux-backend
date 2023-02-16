89dbff60756b2687e0037d48374a80e1693c76a8
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2023-02-15 21:03:05 -0300

    remove unused beforeEach import
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>

--- backend/data/src/test/java/com/erickrodrigues/musicflux/services/GenreServiceImplTest.java
+++ backend/data/src/test/java/com/erickrodrigues/musicflux/services/GenreServiceImplTest.java
@@ -2,7 +2,6 @@ package com.erickrodrigues.musicflux.services;
 
 import com.erickrodrigues.musicflux.domain.Genre;
 import com.erickrodrigues.musicflux.repositories.GenreRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
