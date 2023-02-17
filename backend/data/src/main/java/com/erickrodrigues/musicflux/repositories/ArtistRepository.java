73bca5a7bdc75b7613b443019cd0691123d543d8
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2023-02-16 21:49:45 -0300

    add findAllByNameContainingIgnoreCase to ArtistRepository
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>

--- backend/data/src/main/java/com/erickrodrigues/musicflux/repositories/ArtistRepository.java
+++ backend/data/src/main/java/com/erickrodrigues/musicflux/repositories/ArtistRepository.java
@@ -3,4 +3,9 @@ package com.erickrodrigues.musicflux.repositories;
 import com.erickrodrigues.musicflux.domain.Artist;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ArtistRepository extends CrudRepository<Artist, Long> {}
+import java.util.Set;
+
+public interface ArtistRepository extends CrudRepository<Artist, Long> {
+
+    Set<Artist> findAllByNameContainingIgnoreCase(String name);
+}
