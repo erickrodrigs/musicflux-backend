c8563937dbf8645dde159e95040a0bd038c25341
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2023-02-17 15:59:59 -0300

    add findByUsernameOrEmail and findByUsernameAndPassword methods to UserRepository
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>

--- backend/data/src/main/java/com/erickrodrigues/musicflux/repositories/UserRepository.java
+++ backend/data/src/main/java/com/erickrodrigues/musicflux/repositories/UserRepository.java
@@ -3,4 +3,11 @@ package com.erickrodrigues.musicflux.repositories;
 import com.erickrodrigues.musicflux.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {}
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    Optional<User> findByUsernameOrEmail(String username, String email);
+
+    Optional<User> findByUsernameAndPassword(String username, String password);
+}
