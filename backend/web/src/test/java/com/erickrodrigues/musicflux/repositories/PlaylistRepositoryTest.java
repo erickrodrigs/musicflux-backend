26a28cc02a0d334c4c8bd5bfd3875944c4c3e86a
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2023-02-23 16:58:52 -0300

    add findAllByProfileId tests to PlaylistRepository
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>

--- backend/web/src/test/java/com/erickrodrigues/musicflux/repositories/PlaylistRepositoryTest.java
+++ backend/web/src/test/java/com/erickrodrigues/musicflux/repositories/PlaylistRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.erickrodrigues.musicflux.repositories;
 
 import com.erickrodrigues.musicflux.domain.Playlist;
+import com.erickrodrigues.musicflux.domain.Profile;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +10,7 @@ import org.springframework.test.annotation.DirtiesContext;
 
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
@@ -19,14 +19,21 @@ public class PlaylistRepositoryTest {
     @Autowired
     private PlaylistRepository playlistRepository;
 
+    @Autowired
+    private ProfileRepository profileRepository;
+
+    private Profile profile;
+
     private Playlist playlist1, playlist2, playlist3;
 
     @BeforeEach
     public void setUp() {
-        playlist1 = Playlist.builder().id(1L).name("my fav songs - heavy metal").build();
-        playlist2 = Playlist.builder().id(2L).name("THESE ARE MY FAV SONGS OF ALL TIME").build();
-        playlist3 = Playlist.builder().id(3L).name("funk").build();
+        profile = Profile.builder().id(1L).build();
+        playlist1 = Playlist.builder().id(1L).profile(profile).name("my fav songs - heavy metal").build();
+        playlist2 = Playlist.builder().id(2L).profile(profile).name("THESE ARE MY FAV SONGS OF ALL TIME").build();
+        playlist3 = Playlist.builder().id(3L).profile(profile).name("funk").build();
 
+        profileRepository.save(profile);
         playlistRepository.save(playlist1);
         playlistRepository.save(playlist2);
         playlistRepository.save(playlist3);
@@ -45,5 +52,22 @@ public class PlaylistRepositoryTest {
 
         assertEquals(1, playlists.size());
         assertTrue(playlists.contains(playlist3));
+    }
+
+    @Test
+    public void findAllByProfileId() {
+        final Set<Playlist> playlists = playlistRepository.findAllByProfileId(profile.getId());
+
+        assertEquals(3, playlists.size());
+        assertTrue(playlists.containsAll(Set.of(playlist1, playlist2, playlist3)));
+    }
+
+    @Test
+    public void findAllByProfileIdWhenItDoesNotExist() {
+        final Long unknownProfileId = 4235L;
+        final Set<Playlist> playlists = playlistRepository.findAllByProfileId(unknownProfileId);
+
+        assertNotNull(playlists);
+        assertEquals(0, playlists.size());
     }
 }
