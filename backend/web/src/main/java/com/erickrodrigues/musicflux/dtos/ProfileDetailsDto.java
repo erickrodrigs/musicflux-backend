236436a04f76d9a126723f01ccc49250e098c401
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2023-02-28 19:00:20 -0300

    change lombok annotations in dtos
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>

--- backend/web/src/main/java/com/erickrodrigues/musicflux/dtos/ProfileDetailsDto.java
+++ backend/web/src/main/java/com/erickrodrigues/musicflux/dtos/ProfileDetailsDto.java
@@ -1,10 +1,14 @@
 package com.erickrodrigues.musicflux.dtos;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Getter
-@Setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ProfileDetailsDto {
 
     private Long id;
