e7f8f9a027b6c7d9f5b136890448d11813bc7870
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2023-03-05 20:15:47 -0300

    add custom exceptions
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>

---
+++ backend/data/src/main/java/com/erickrodrigues/musicflux/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.erickrodrigues.musicflux.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
