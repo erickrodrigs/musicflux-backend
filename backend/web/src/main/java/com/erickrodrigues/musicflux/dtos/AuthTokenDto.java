0fcb89d1119a4d34c006c25a7f5a1260a2c4d64b
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2023-03-06 20:49:04 -0300

    create auth token dto
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>

---
+++ backend/web/src/main/java/com/erickrodrigues/musicflux/dtos/AuthTokenDto.java
@@ -0,0 +1,15 @@
+package com.erickrodrigues.musicflux.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuthTokenDto {
+
+    private String token;
+}
