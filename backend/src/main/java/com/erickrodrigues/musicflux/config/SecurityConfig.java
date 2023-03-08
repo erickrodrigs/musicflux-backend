e204c1a5d768ce1e7c4052914125c6cda7ae2fbc
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2023-03-08 17:34:09 -0300

    refactor authenticated endpoints in security config
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>

--- backend/src/main/java/com/erickrodrigues/musicflux/config/SecurityConfig.java
+++ backend/src/main/java/com/erickrodrigues/musicflux/config/SecurityConfig.java
@@ -13,6 +13,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -26,19 +28,17 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf()
+                .ignoringRequestMatchers(toH2Console())
                 .disable()
+                .headers()
+                .frameOptions()
+                .disable()
+                .and()
                 .authorizeHttpRequests()
-                .requestMatchers(
-                        "/auth/**",
-                        "/swagger-ui/**",
-                        "/v3/api-docs/**",
-                        "/artists/**",
-                        "/albums/**",
-                        "/catalogue/**"
-                )
-                .permitAll()
-                .anyRequest()
+                .requestMatchers("/users/me/**")
                 .authenticated()
+                .anyRequest()
+                .permitAll()
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
