90056b9f0b88baeaad725efa3fa17ee614a43800
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2023-03-09 20:25:41 -0300

    add list of dtos mapping to favorites
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>

--- backend/src/main/java/com/erickrodrigues/musicflux/favorite/FavoriteMapper.java
+++ backend/src/main/java/com/erickrodrigues/musicflux/favorite/FavoriteMapper.java
@@ -4,9 +4,13 @@ import com.erickrodrigues.musicflux.song.SongMapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring", uses = {SongMapper.class})
 public interface FavoriteMapper {
 
     @Mapping(target = "userId", source = "favorite.user.id")
     FavoriteDetailsDto toFavoriteDetailsDto(Favorite favorite);
+
+    List<FavoriteDetailsDto> toListOfFavoriteDetailsDto(List<Favorite> favorites);
 }
