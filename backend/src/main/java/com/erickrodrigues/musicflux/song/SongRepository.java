bb69125c46b5402ab7a0873906868f04edb0b400
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2023-03-09 19:11:49 -0300

    add findAllByGenresNameIgnoreCase method to song repository
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>

--- backend/src/main/java/com/erickrodrigues/musicflux/song/SongRepository.java
+++ backend/src/main/java/com/erickrodrigues/musicflux/song/SongRepository.java
@@ -1,6 +1,5 @@
 package com.erickrodrigues.musicflux.song;
 
-import com.erickrodrigues.musicflux.song.Song;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -8,4 +7,6 @@ import java.util.List;
 public interface SongRepository extends CrudRepository<Song, Long> {
 
     List<Song> findAllByTitleContainingIgnoreCase(String title);
+
+    List<Song> findAllByGenresNameIgnoreCase(String genreName);
 }
