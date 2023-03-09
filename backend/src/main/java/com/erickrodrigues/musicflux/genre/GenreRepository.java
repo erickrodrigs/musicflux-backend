296ad1cb10f90498ec1bfbfff83b45656af6bd1f
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2023-03-09 19:17:15 -0300

    remove useless import from genre service and repository
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>

--- backend/src/main/java/com/erickrodrigues/musicflux/genre/GenreRepository.java
+++ backend/src/main/java/com/erickrodrigues/musicflux/genre/GenreRepository.java
@@ -1,6 +1,5 @@
 package com.erickrodrigues.musicflux.genre;
 
-import com.erickrodrigues.musicflux.genre.Genre;
 import org.springframework.data.repository.CrudRepository;
 
 public interface GenreRepository extends CrudRepository<Genre, Long> {}
