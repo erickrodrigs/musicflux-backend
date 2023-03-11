5ca7d17c3e6b926b4f7831a5470131bd681bc2f2
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2023-03-10 21:58:31 -0300

    add authenticate user integration tests
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>

---
+++ backend/src/test/java/com/erickrodrigues/musicflux/integration/AuthenticateUserTest.java
@@ -0,0 +1,82 @@
+package com.erickrodrigues.musicflux.integration;
+
+import com.erickrodrigues.musicflux.auth.AuthCredentialsDto;
+import com.erickrodrigues.musicflux.auth.AuthRegistrationDto;
+import com.erickrodrigues.musicflux.auth.AuthTokenDto;
+import com.erickrodrigues.musicflux.handlers.ApiError;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(classes = ITConfig.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class AuthenticateUserTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @BeforeAll
+    public void setUp() {
+        final AuthRegistrationDto authRegistrationDto = AuthRegistrationDto
+                .builder()
+                .name("Erick")
+                .username("erick123")
+                .email("erick@erick.com")
+                .password("erick123")
+                .build();
+
+        restTemplate.postForEntity(
+                getUrl() + "/auth/register",
+                authRegistrationDto,
+                AuthTokenDto.class
+        );
+    }
+
+    @Test
+    public void authenticateUser() {
+        final AuthCredentialsDto authCredentialsDto = AuthCredentialsDto
+                .builder()
+                .username("erick123")
+                .password("erick123")
+                .build();
+        final ResponseEntity<AuthTokenDto> response = restTemplate.postForEntity(
+                getUrl() + "/auth",
+                authCredentialsDto,
+                AuthTokenDto.class
+        );
+
+        assertEquals(200, response.getStatusCode().value());
+        assertNotNull(response.getBody());
+        assertNotNull(response.getBody().getToken());
+    }
+
+    @Test
+    public void authenticateUserWhenInfoAreInvalid() {
+        final AuthCredentialsDto authCredentialsDto = AuthCredentialsDto
+                .builder()
+                .username("erick123")
+                .password("carlos")
+                .build();
+
+        assertThrows(HttpClientErrorException.Unauthorized.class, () -> restTemplate.postForEntity(
+                getUrl() + "/auth",
+                authCredentialsDto,
+                ApiError.class
+        ));
+    }
+
+    private String getUrl() {
+        return "http://localhost:" + port;
+    }
+}
