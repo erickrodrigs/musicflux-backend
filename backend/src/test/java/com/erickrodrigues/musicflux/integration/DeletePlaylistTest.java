819aa1527725e870f0f7d70eb07aa8c420bd9214
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2023-03-13 15:05:20 -0300

    add integration tests for 'delete playlist' use case
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>

---
+++ backend/src/test/java/com/erickrodrigues/musicflux/integration/DeletePlaylistTest.java
@@ -0,0 +1,87 @@
+package com.erickrodrigues.musicflux.integration;
+
+import com.erickrodrigues.musicflux.auth.AuthCredentialsDto;
+import com.erickrodrigues.musicflux.auth.AuthTokenDto;
+import org.apache.hc.core5.http.HttpHeaders;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(classes = ITConfig.class)
+@Sql({"/data-test.sql"})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class DeletePlaylistTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @BeforeEach
+    public void setUp() {
+        final AuthCredentialsDto authCredentialsDto = AuthCredentialsDto
+                .builder()
+                .username("erickrodrigs")
+                .password("carlos123")
+                .build();
+        final ResponseEntity<AuthTokenDto> response = restTemplate.postForEntity(
+                getBaseUrl() + "/auth",
+                authCredentialsDto,
+                AuthTokenDto.class
+        );
+
+        String token = Objects.requireNonNull(response.getBody()).getToken();
+
+        restTemplate.getInterceptors().add((outReq, bytes, clientHttpReqExec) -> {
+            outReq.getHeaders().set(
+                    HttpHeaders.AUTHORIZATION, "Bearer " + token
+            );
+
+            return clientHttpReqExec.execute(outReq, bytes);
+        });
+    }
+
+    @Test
+    public void deletePlaylist() {
+        final long playlistId = 1L;
+        final ResponseEntity<Object> response = restTemplate.exchange(
+                getBaseUrl() + "/users/me/playlists/" + playlistId,
+                HttpMethod.DELETE,
+                null,
+                Object.class
+        );
+
+        assertEquals(200, response.getStatusCode().value());
+        assertNull(response.getBody());
+    }
+
+    @Test
+    public void deletePlaylistWhenItDoesNotExist() {
+        final long playlistId = 498L;
+        assertThrows(HttpClientErrorException.NotFound.class, () -> restTemplate.exchange(
+                getBaseUrl() + "/users/me/playlists/" + playlistId,
+                HttpMethod.DELETE,
+                null,
+                Object.class
+        ));
+    }
+
+    private String getBaseUrl() {
+        return "http://localhost:" + port;
+    }
+}
