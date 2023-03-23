1e972f1b2c00ee76bf63a2f96482802127bc167a
erickrodrigs <erick.santana@opus-software.com.br>
2023-03-23 15:53:08 -0300

    refactor song controller and play song test
    Co-authored-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>
    Co-authored-by: Erick Rodrigues <erick.santana@opus-software.com.br>

--- backend/src/main/java/com/erickrodrigues/musicflux/song/SongController.java
+++ backend/src/main/java/com/erickrodrigues/musicflux/song/SongController.java
@@ -4,8 +4,16 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -18,12 +26,16 @@ public class SongController {
     private final SongMapper songMapper;
 
     @Operation(summary = "Play a song by its id")
-    @PutMapping("/users/me/songs/{song_id}/play")
-    @ResponseStatus(HttpStatus.OK)
-    public void playSong(HttpServletRequest request,
-                         @PathVariable("song_id") Long songId) {
+    @GetMapping("/users/me/songs/{song_id}")
+    public ResponseEntity<Resource> playSong(HttpServletRequest request,
+                                   @PathVariable("song_id") Long songId) {
         final Long userId = (Long) request.getAttribute("userId");
-        songService.play(userId, songId);
+        final Song song = songService.play(userId, songId);
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.parseMediaType("audio/mpeg3"))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + song.getTitle() + ".mp3\"")
+                .body(new ByteArrayResource(song.getData()));
     }
 
     @Operation(summary = "Get all songs in an album by its id")
