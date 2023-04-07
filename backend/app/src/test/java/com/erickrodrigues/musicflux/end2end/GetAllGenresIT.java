19db5c3a4c08bc18ac5b0449642e3172e0f77d36
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2023-04-07 00:28:31 -0300

    add end2end tests for 'get all genres' use case
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>

---
+++ backend/app/src/test/java/com/erickrodrigues/musicflux/end2end/GetAllGenresIT.java
@@ -0,0 +1,41 @@
+package com.erickrodrigues.musicflux.end2end;
+
+import com.erickrodrigues.musicflux.genre.GenreDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.web.client.RestTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql({"/data-test.sql"})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class GetAllGenresIT {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Test
+    public void shouldGetAllGenres() {
+        // given
+        final String url = "http://localhost:" + port + "/genres";
+
+        // when
+        final ResponseEntity<GenreDto[]> response = restTemplate.getForEntity(url, GenreDto[].class);
+
+        // then
+        assertNotNull(response.getBody());
+        assertEquals(200, response.getStatusCode().value());
+        assertTrue(response.getBody().length > 0);
+    }
+}
