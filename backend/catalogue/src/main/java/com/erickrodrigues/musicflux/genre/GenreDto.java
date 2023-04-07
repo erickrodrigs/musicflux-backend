5293108998440ae37c59dceb94577790a6c2f382
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2023-04-07 00:19:19 -0300

    create genre dto and mapper
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>

---
+++ backend/catalogue/src/main/java/com/erickrodrigues/musicflux/genre/GenreDto.java
@@ -0,0 +1,17 @@
+package com.erickrodrigues.musicflux.genre;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GenreDto {
+
+    private Long id;
+
+    private String name;
+}
