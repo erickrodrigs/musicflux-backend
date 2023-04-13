11b2503d3f67933fa0c50878bf5cdcbd8b73bdba
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2023-04-13 16:31:27 -0300

    add swagger annotation to getArtist method + use request mapping annotation
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>

--- backend/catalogue/src/main/java/com/erickrodrigues/musicflux/artist/ArtistController.java
+++ backend/catalogue/src/main/java/com/erickrodrigues/musicflux/artist/ArtistController.java
@@ -8,15 +8,13 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Tag(name = "artists")
 @RestController
+@RequestMapping("/artists/{artist_id}")
 @RequiredArgsConstructor
 public class ArtistController {
 
@@ -25,21 +23,22 @@ public class ArtistController {
     private final AlbumMapper albumMapper;
     private final TrackMapper trackMapper;
 
-    @GetMapping("/artists/{artist_id}")
+    @Operation(summary = "Get artist by their id")
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public ArtistDetailsDto getArtist(@PathVariable("artist_id") Long artistId) {
         return artistMapper.toArtistDetailsDto(artistService.findById(artistId));
     }
 
     @Operation(summary = "Get artist's albums by their id")
-    @GetMapping("/artists/{artist_id}/albums")
+    @GetMapping("/albums")
     @ResponseStatus(HttpStatus.OK)
     public List<AlbumDetailsDto> getArtistAlbums(@PathVariable("artist_id") Long artistId) {
         return albumMapper.toListOfAlbumDetailsDto(artistService.getArtistAlbums(artistId));
     }
 
     @Operation(summary = "Get artist's top tracks by their id")
-    @GetMapping("/artists/{artist_id}/top-tracks")
+    @GetMapping("/top-tracks")
     @ResponseStatus(HttpStatus.OK)
     public List<TrackDto> getArtistTopTracks(@PathVariable("artist_id") Long artistId) {
         return trackMapper.toListOfTrackDetailsDto(artistService.getTopTracks(artistId));
